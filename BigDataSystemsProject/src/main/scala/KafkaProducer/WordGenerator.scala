7f23e30e033dd78d8fab78fd2cd52d81f8c0e58a
Aggeliki Romanou <agromanou@gmail.com>
2017-06-07 03:26:12 +0300

    Adds inital dummy scripts for each different utility

---
+++ BigDataSystemsProject/src/main/scala/KafkaProducer/WordGenerator.scala
@@ -0,0 +1,33 @@
+package KafkaProducer
+
+import org.apache.kafka.clients.producer._
+
+/**
+  * Created by aggelikiromanou on 07/06/2017.
+  */
+
+object WordGenerator extends App {
+
+  import java.util.Properties
+
+
+  val  props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  val producer = new KafkaProducer[String, String](props)
+
+  val TOPIC="test"
+
+  for(i<- 1 to 50){
+    val record = new ProducerRecord(TOPIC, "key", s"hello $i")
+    producer.send(record)
+  }
+
+  val record = new ProducerRecord(TOPIC, "key", "the end "+new java.util.Date)
+  producer.send(record)
+
+  producer.close()
+}
